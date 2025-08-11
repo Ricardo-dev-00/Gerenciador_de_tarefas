22b6e4bb78093581ba61a71b8b8146d2ccedb982
Ricardo-dev-00 <ricardo.dev.of@gmail.com>
2025-08-11 19:47:17 -0300

    ajuste .gitignore para não versionar node_modules, dist e arquivos temporários

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Exerc-cio-m-dulo-17--Gerenciador_de_Tarefas/',
   plugins: [react()],
   root: './src',
   build: {
